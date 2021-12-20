20636ce17ee1f6fb95c167cb50a60cb9a25029fc
hamster <dokalov@outlook.com>
2021-12-20 21:29:11 +0300

    digit powered

---
+++ Task000/Program.cs
@@ -0,0 +1,9 @@
+﻿//Console.Write("Введите число: ");
+//int numberA = Console.Read();
+//Console.WriteLine(numberA * numberA);
+// int a = Console.Read();
+// int b = a ^ 2;
+// Console.WriteLine(b);
+string a = Console.ReadLine();
+int b = Convert.ToInt32(a);
+Console.WriteLine(b*b);
