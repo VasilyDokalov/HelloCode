92ae0304484326e5abf5804e73bb8a8c56c0ced3
hamster <dokalov@outlook.com>
2021-12-20 21:53:31 +0300

    Сравнение квадрата

---
+++ Task001/Program.cs
@@ -0,0 +1,13 @@
+﻿string a = Console.ReadLine();
+int b = Convert.ToInt32(a);
+b = b * b;
+string c = Console.ReadLine();
+int d = Convert.ToInt32(c);
+if(b == d)
+{
+    Console.WriteLine("Первое является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое не является квадратом второго");
+}
