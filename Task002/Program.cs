7183729cc4aa6549ae55f1f26b8d861c470d9f2b
hamster <dokalov@outlook.com>
2021-12-23 21:14:18 +0300

    task002 solved

---
+++ Task002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("a= ");
+string a = Console.ReadLine();
+int c = Convert.ToInt32(a);
+Console.Write("b= ");
+string b = Console.ReadLine();
+int d = Convert.ToInt32(b);
+if(c > d)
+{
+    Console.WriteLine("a > b");
+}
+else
+{
+    Console.WriteLine("a < b");
+}
